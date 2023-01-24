ae978fce6c877e80454e957d311091a8f11e380f
Lera <jgfnvb@mail.ru>
2023-01-24 22:54:30 +0700

    delete auth validation

--- src/routes/auth_router.ts
+++ src/routes/auth_router.ts
@@ -1,17 +1,18 @@
 import {Router,Request,Response} from "express";
 import {userService} from "../service/user-service";
-import {
+/*import {
     userInputEmailValidation,
     userInputLoginValidation,
     userInputPasswordValidation
-} from "../MiddleWares/input-user-validation";
+} from "../MiddleWares/input-user-validation";*/
 
 export const authRouter = Router({});
-authRouter.post('/login',userInputLoginValidation,userInputEmailValidation,userInputPasswordValidation, async (req:Request,res:Response)=>{
+authRouter.post('/login', async (req:Request,res:Response)=>{
    const checkResult:boolean = await userService.checkLoginAndPassword(req.body.loginOrEmail, req.body.password)
     if(checkResult){
         res.sendStatus(201)
     }
+    //userInputLoginValidation,userInputEmailValidation,userInputPasswordValidation,
     else{
         res.sendStatus(401)
     }
