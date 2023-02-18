3ba58bcaa70d0aa00eee553338f5ca822dcaf5f4
Lera <jgfnvb@mail.ru>
2023-02-18 22:38:04 +0700

    , {}, {content: string} checkOwnerOfComments

--- src/MiddleWares/input-comment-validation.ts
+++ src/MiddleWares/input-comment-validation.ts
@@ -1,7 +1,7 @@
 const {body} = require('express-validator');
 
-export const CommentInputContentValidation = body('content').isString().withMessage('content should be string')
-    .trim().exists({checkFalsy: true}).withMessage('Title should exist').bail().isLength({min:20, max:300})
+export const CommentInputContentValidation = body('content').isString().trim().withMessage('content should be string')
+    .exists({checkFalsy: true}).withMessage('Title should exist').bail().isLength({min:20, max:300})
     .withMessage('content should be from 20 to 300 symbols').bail()
 
 
