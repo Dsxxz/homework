26dcc95b42b4e9328c33ba972a194e9d0631a4b1
Lera <jgfnvb@mail.ru>
2023-02-18 22:36:46 +0700

    , {}, {content: string}

--- src/routes/comments_router.ts
+++ src/routes/comments_router.ts
@@ -1,4 +1,4 @@
-import { Router} from "express";
+import { Router,Request,Response} from "express";
 import {commentsRepository} from "../repositories/comments_in_db_repository";
 import {authMiddleWare} from "../MiddleWares/auth-middleWare";
 import {checkOwnerOfComments} from "../MiddleWares/checkOwnerOfComments";
@@ -17,7 +17,11 @@ commentsRouter.get('/:id',async (req,res)=>{
     res.status(200).send(findComment);
     return;
 })
-commentsRouter.delete('/:id',checkOwnerOfComments,authMiddleWare,inputCommentsValidation,async (req,res)=>{
+commentsRouter.delete('/:id',
+    checkOwnerOfComments,
+    authMiddleWare,
+    inputCommentsValidation,
+    async (req,res)=>{
     const findComment = await commentsRepository.deleteComment(req.params.id)
     if(!findComment){
         res.sendStatus(404);
@@ -26,7 +30,11 @@ commentsRouter.delete('/:id',checkOwnerOfComments,authMiddleWare,inputCommentsVa
     res.sendStatus(204);
     return;
 })
-commentsRouter.put('/:id',CommentInputContentValidation, authMiddleWare,checkOwnerOfComments,inputCommentsValidation, async (req, res)=>{
+commentsRouter.put('/:id',
+    CommentInputContentValidation,
+    authMiddleWare,checkOwnerOfComments,
+    inputCommentsValidation,
+    async (req:Request<{ id: string }, {}, {content: string}>, res:Response)=>{
     const findComment = await commentsRepository.updateComment(req.params.id,req.body.content)
     if(!findComment){
         res.sendStatus(404)
