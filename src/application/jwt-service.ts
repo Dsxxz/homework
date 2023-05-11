6cfaa517981235e7a15eebd516cae25868a7e302
Lera <jgfnvb@mail.ru>
2023-05-11 18:44:57 +0700

    time

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -4,7 +4,7 @@ import {UserAccountDbType} from "../models/userType";
 
 export const jwtService={
     async createAccess(user:UserAccountDbType){
-        const token = jwt.sign({userID:user._id}, "JWT_Secret",{expiresIn:'100 s'})
+        const token = jwt.sign({userID:user._id}, "JWT_Secret",{expiresIn:'10s'})
         return {
             resultCode:0,
             data:{
@@ -22,6 +22,6 @@ export const jwtService={
         }
     },
     async  createRefresh (user: UserAccountDbType){
-        return jwt.sign({userID:user._id}, 'refreshTokenPrivateKey', {expiresIn:'100'});
+        return jwt.sign({userID:user._id}, 'refreshTokenPrivateKey', {expiresIn:'20s'});
     }
 }
