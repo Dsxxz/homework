15d55f6fc8b953fe5b0bd44f3e0e93dceeb13980
Lera <jgfnvb@mail.ru>
2023-05-12 15:49:37 +0700

    10ы

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -3,7 +3,7 @@ import jwt from  'jsonwebtoken';
 
 export const jwtService={
     async createAccess(id:ObjectId){
-        const token = jwt.sign({userID:id}, "JWT_Secret",{expiresIn:'1000'})
+        const token = jwt.sign({userID:id}, "JWT_Secret",{expiresIn:'10s'})
         return {
             resultCode:0,
             data:{
@@ -20,7 +20,7 @@ export const jwtService={
                 return null;}
     },
     async  createRefresh (id:ObjectId){
-        return jwt.sign({userID:id}, 'refreshTokenPrivateKey', {expiresIn:'2000'});
+        return jwt.sign({userID:id}, 'refreshTokenPrivateKey', {expiresIn:'20s'});
     },
     async verifyUserIdByRefreshToken(token:string):Promise<ObjectId|null>{
         try {
