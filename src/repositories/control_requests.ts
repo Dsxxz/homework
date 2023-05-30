f595c5deaf0c2fe2053fd84d0e112f337a8fc0a0
Lera <jgfnvb@mail.ru>
2023-05-30 23:16:11 +0700

    //

--- src/repositories/control_requests.ts
+++ src/repositories/control_requests.ts
@@ -5,7 +5,7 @@ export const controlRequests={
         await IPRestrictCollectionDb.insertOne({ip,url,date})
     },
     async checkRequest(ip:string, url:string, date:Date){
-        await IPRestrictCollectionDb.deleteMany({date:{$lt:date}})
+        //await IPRestrictCollectionDb.deleteMany({date:{$lt:date}})
         return await IPRestrictCollectionDb.countDocuments({ip,url,date:{$gte:date}})
     }
 }
