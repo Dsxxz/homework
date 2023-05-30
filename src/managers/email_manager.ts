8a0ce70dad26bf373f6b3db1d2f905072b612f72
Lera <jgfnvb@mail.ru>
2023-05-31 00:47:27 +0700

    delete async await

--- src/managers/email_manager.ts
+++ src/managers/email_manager.ts
@@ -1,8 +1,8 @@
 import {emailAdapter} from "../adapters/email_adapter";
 import {UserAccountDbType} from "../models/userType";
 export const emailManager = {
-    async sendEmailConfirmationCode(user:UserAccountDbType){
-        await emailAdapter.sendEmail(user.accountData.email,'password recovery',
+     sendEmailConfirmationCode(user:UserAccountDbType){
+         emailAdapter.sendEmail(user.accountData.email,'password recovery',
             '<h1>Thank for your registration</h1>\n' +
             `<p>${user.emailConfirmation.confirmationCode} To finish registration please follow the link below:\n` +
             `<a href="https://somesite.com/confirm-email?code=${user.emailConfirmation.confirmationCode}">complete registration</a>\n` +
